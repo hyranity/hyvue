25243af226a40695d0b351cdbb069c7b5c112427
Johann Lee <mast3r1sgr3at@hotmail.com>
2021-08-05 20:20:17 +0800

    init

---
+++ src/components/Playground.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="playground">
+    <h3>{{ title }}</h3>
+
+    <div class="playground__container"><playground-component /></div>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PlaygroundComponent from "./PlaygroundComponent.vue";
+
+export default defineComponent({
+  setup() {},
+  data() {
+    return {
+      title: "Component Playground",
+      description:
+        "Above is a playground where you can test VueJS component designs. Or you can simply remove the page and start your own project.",
+    };
+  },
+  components: { PlaygroundComponent },
+});
+</script>
+
+<style lang="scss" scoped>
+.playground {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & h3 {
+    margin: 0;
+    margin-top: 40px;
+    margin-bottom: 5px;
+  }
+
+  & p {
+    padding: 0;
+    margin: 0;
+    margin-top: 10px;
+    opacity: 0.8;
+  }
+}
+
+.playground__container {
+  border: 1px solid grey;
+  width: 1000px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/PlaygroundComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="playgroundComponent">Playground Component</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.playgroundComponent {
+  background-color: lightgray;
+  padding: 10px;
+  border-radius: 4px;
+  font-weight: 700;
+}
+</style>
